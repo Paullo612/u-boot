50052b83ee7649937b90dc780fedb639677cc9ce
Jonas Karlman <jonas@kwiboo.se>
2026-02-06 00:23:28 +0000

    mmc: rockchip_sdhci: Add support for RK3562
    Add support for SDHCI controller in RK3562.
    
    Variant tap and delay num is copied from vendor Linux tag
    linux-6.1-stan-rkr6.
    
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/mmc/rockchip_sdhci.c
+++ drivers/mmc/rockchip_sdhci.c
@@ -687,6 +687,18 @@ static const struct sdhci_data rk3528_data = {
 	.ddr50_strbin_delay_num = 0xa,
 };
 
+static const struct sdhci_data rk3562_data = {
+	.set_ios_post = rk3568_sdhci_set_ios_post,
+	.set_clock = rk3568_sdhci_set_clock,
+	.config_dll = rk3568_sdhci_config_dll,
+	.flags = FLAG_TAPVALUE_FROM_SW,
+	.hs200_txclk_tapnum = 0xc,
+	.hs400_txclk_tapnum = 0x6,
+	.hs400_cmdout_tapnum = 0x6,
+	.hs400_strbin_tapnum = 0x3,
+	.ddr50_strbin_delay_num = 0xa,
+};
+
 static const struct sdhci_data rk3568_data = {
 	.set_ios_post = rk3568_sdhci_set_ios_post,
 	.set_clock = rk3568_sdhci_set_clock,
@@ -729,6 +741,10 @@ static const struct udevice_id sdhci_ids[] = {
 	{
 		.compatible = "rockchip,rk3528-dwcmshc",
 		.data = (ulong)&rk3528_data,
+	},
+	{
+		.compatible = "rockchip,rk3562-dwcmshc",
+		.data = (ulong)&rk3562_data,
 	},
 	{
 		.compatible = "rockchip,rk3568-dwcmshc",
