65ba441c767fecf611a0853d0f832776d0d4d9b0
Jonas Karlman <jonas@kwiboo.se>
2026-05-24 11:22:37 +0000

    WIP: rockchip: sdram: optimize get_info() ops for rk3588
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/ram/rockchip/sdram_rk3588.c
+++ drivers/ram/rockchip/sdram_rk3588.c
@@ -3,37 +3,20 @@
  * (C) Copyright 2021 Rockchip Electronics Co., Ltd.
  */
 
-#include <config.h>
 #include <dm.h>
 #include <ram.h>
-#include <syscon.h>
-#include <asm/arch-rockchip/clock.h>
 #include <asm/arch-rockchip/grf_rk3588.h>
 #include <asm/arch-rockchip/sdram.h>
 
-struct dram_info {
-	struct ram_info info;
-	struct rk3588_pmu1grf *pmugrf;
-};
-
-static int rk3588_dmc_probe(struct udevice *dev)
-{
-	struct dram_info *priv = dev_get_priv(dev);
-
-	priv->pmugrf = syscon_get_first_range(ROCKCHIP_SYSCON_PMUGRF);
-	priv->info.base = CFG_SYS_SDRAM_BASE;
-	priv->info.size =
-		rockchip_sdram_size((phys_addr_t)&priv->pmugrf->os_reg[2]) +
-		rockchip_sdram_size((phys_addr_t)&priv->pmugrf->os_reg[4]);
-
-	return 0;
-}
+#define PMU1GRF_BASE			0xfd58a000
 
 static int rk3588_dmc_get_info(struct udevice *dev, struct ram_info *info)
 {
-	struct dram_info *priv = dev_get_priv(dev);
+	static struct rk3588_pmu1grf * const pmugrf = (void *)PMU1GRF_BASE;
 
-	*info = priv->info;
+	info->base = CFG_SYS_SDRAM_BASE;
+	info->size = rockchip_sdram_size((phys_addr_t)&pmugrf->os_reg[2]) +
+		     rockchip_sdram_size((phys_addr_t)&pmugrf->os_reg[4]);
 
 	return 0;
 }
@@ -47,11 +30,9 @@ static const struct udevice_id rk3588_dmc_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(dmc_rk3588) = {
+U_BOOT_DRIVER(rockchip_rk3588_dmc) = {
 	.name = "rockchip_rk3588_dmc",
 	.id = UCLASS_RAM,
 	.of_match = rk3588_dmc_ids,
 	.ops = &rk3588_dmc_ops,
-	.probe = rk3588_dmc_probe,
-	.priv_auto = sizeof(struct dram_info),
 };
