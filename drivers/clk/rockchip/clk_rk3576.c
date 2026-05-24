ae62dfb757b30cf71679074a16716e6e5a9dc91b
Jonas Karlman <jonas@kwiboo.se>
2026-05-24 23:10:14 +0000

    WIP: rockchip: clk: use rockchip_sysreset_bind() helper for rk3576
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/clk/rockchip/clk_rk3576.c
+++ drivers/clk/rockchip/clk_rk3576.c
@@ -2469,22 +2469,12 @@ static int rk3576_clk_ofdata_to_platdata(struct udevice *dev)
 static int rk3576_clk_bind(struct udevice *dev)
 {
 	int ret;
-	struct udevice *sys_child;
-	struct sysreset_reg *priv;
 
 	/* The reset driver does not have a device node, so bind it here */
-	ret = device_bind_driver(dev, "rockchip_sysreset", "sysreset",
-				 &sys_child);
-	if (ret) {
+	ret = rockchip_sysreset_bind(dev, RK3576_CRU_BASE, RK3576_GLB_RST_ST,
+				     RK3576_GLB_SRST_FST, RK3576_GLB_SRST_SND);
+	if (ret)
 		debug("Warning: No sysreset driver: ret=%d\n", ret);
-	} else {
-		priv = malloc(sizeof(struct sysreset_reg));
-		priv->glb_srst_fst_value = offsetof(struct rk3576_cru,
-						    glb_srst_fst);
-		priv->glb_srst_snd_value = offsetof(struct rk3576_cru,
-						    glb_srsr_snd);
-		dev_set_priv(sys_child, priv);
-	}
 
 #if CONFIG_IS_ENABLED(RESET_ROCKCHIP)
 	ret = offsetof(struct rk3576_cru, softrst_con[0]);
