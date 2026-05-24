ff2d337b19ca24efbbf04ecfd7da78d73a239db4
Jonas Karlman <jonas@kwiboo.se>
2026-05-24 23:35:08 +0000

    WIP: rockchip: clk: remove rockchip_get_clk() helper for rk3576
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- arch/arm/mach-rockchip/rk3576/clk_rk3576.c
+++ arch/arm/mach-rockchip/rk3576/clk_rk3576.c
@@ -3,14 +3,8 @@
  * (C) Copyright 2020 Rockchip Electronics Co., Ltd.
  */
 
-#include <dm.h>
+#include <linux/kernel.h>
 #include <asm/arch-rockchip/cru_rk3576.h>
-
-int rockchip_get_clk(struct udevice **devp)
-{
-	return uclass_get_device_by_driver(UCLASS_CLK,
-				DM_DRIVER_GET(rockchip_rk3576_cru), devp);
-}
 
 void *rockchip_get_cru(void)
 {
